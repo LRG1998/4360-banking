653be164ac65fdfa77525847359ecd44a0fbfba4
Echamma <elias.john.chamma@gmail.com>
2024-09-17 08:06:29 -0500

    nevermind it was my mistake

--- executable.py
+++ executable.py
@@ -5,108 +5,109 @@ import transaction
 import auth
 from time import sleep
 
-# Constant values
-MAIN_MENU_OPTIONS = ["1", "2", "3"]
-ACCOUNT_MENU_OPTIONS = ["1", "2", "3", "4", "5", "q"]
-
-def clear_screen() -> None:
-    """Clear the screen"""
+def clear():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
 
-def main() -> None:
+def main():
     print("Welcome to LoTek Bank!" ," \n\n\n\n\n")
     print("1) Login to an account! " , "\n")
     print("2) Create an accounts!", "\n")
     print("3) Quit","\n")
 
     response = input("Please pick an option: ")
-    handle_main_menu_response(response)
+    mainHandle(response)
 
-def handle_main_menu_response(response: str) -> None:
-    """Handle the main menu response"""
+def mainHandle(response):
     match response:
         case "1":
-            clear_screen()
+            clear()
             index.login()
-            clear_screen()
-            account_menu()
+            clear()
+            accountMenu()
         case "2":
-            clear_screen()
-            create_account()
+            clear()
+            createMenu()
         case "3" : 
             index.quit()
         case _:
-            clear_screen()
+            clear()
             main()
 
-def create_account() -> None:
-    """Create a new account"""
-    new_username = input("Enter a username: ")
-    new_password = input("Enter your password: ")
-    if not auth.valid(new_username, new_password):
-        new_name = input("Enter your name: ")
-        initial_balance = float(input("Enter the starter balance: "))
-        index.accountdb.create(new_username, new_name, new_password, initial_balance)
-        index.set_new_active()
-        clear_screen()
-        account_menu()
+def createMenu():
+    newUName = input("Enter a Username: ")
+    newPassword = input("Enter your password: ")
+    if not auth.valid(newUName, newPassword):
+        newName = input("Enter your name: ")
+        initBalance = float(input("Enter the starter balance: "))
+        index.accountdb.create(newUName,newName,newPassword,initBalance)
+        index.setNewActive()
+        clear()
+        accountMenu()
     else:
-        clear_screen()
+        clear()
         main()
 
-def account_menu() -> None:
-    """Account menu"""
-    print(index.get_active().Name, "             ", index.get_active().Balance)
+def accountMenu():
+    print(index.getActive().Name, "             ", index.getActive().Balance)
     print("\n" * 10)
     print(" 1) Deposit \n 2) Withdraw \n 3) Transfer \n 4) Close (Delete Account) \n 5) Logout \n \n Q) Quit")
 
-    account_menu_response = input("Please pick an option:  ")
-    handle_account_menu_response(account_menu_response)
+    accResponse = input("Please pick an option:  ")
+    accountHandle(accResponse)
 
-def handle_account_menu_response(response: str) -> None:
-    """Handle the account menu response"""
-    clear_screen()
+def accountHandle(response):
     match response.lower():
         case "1":
-            deposit_menu()
+            clear()
+            depMenu()
+            pass
         case "2":
-            withdraw_menu()
+            clear()
+            withdrawMenu()
+            pass
         case "3":
-            transfer_menu()
+            transferMenu()
+            pass
         case "4":
-            index.accountdb.accounts.pop(index.get_active())
+            index.accountdb.accounts.pop(index.getActive())
+            pass
         case "5":
-            index.clear_active()
+            index.clearActive()
+            clear()
             main()
+            pass
         case "q":
             index.quit()
+            pass
         case _:
-            account_menu()
+            clear()
+            accountMenu()
+            pass
 
-def transfer_menu() -> None:
-    """Transfer menu"""
-    to_account = input("Who are you transferring to? ")
+
+def transferMenu():
+    toAcc = input("Who are you transferring to? ")
     amount = input("How much do you want to transfer? ")
-    transaction.transfer(index.get_active(), index.accountdb.accounts[index.accountdb.fetch(to_account)], amount)
-    clear_screen()
-    account_menu()
+    transaction.transfer(index.getActive(),index.accountdb.accounts[index.accountdb.fetch(toAcc)],amount)
+    clear()
+    accountMenu()
 
-def deposit_menu() -> None:
-    """Deposit menu"""
+
+
+def depMenu():
     amount = input("Enter amount to deposit: ")
-    transaction.deposit(index.get_active(), amount)
-    clear_screen()
-    account_menu()
+    transaction.deposit(index.getActive(),amount)
+    clear()
+    accountMenu()
 
-def withdraw_menu() -> None:
-    """Withdraw menu"""
+def withdrawMenu():
     amount = input("How much do you want to withdraw? ")
-    transaction.withdraw(index.get_active(), amount)
-    clear_screen()
-    account_menu()
+    transaction.withdraw(index.getActive(), amount)
+    clear()
+    accountMenu()
 
 if __name__ == "__main__":
     main()
